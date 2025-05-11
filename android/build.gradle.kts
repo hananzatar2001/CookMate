bfd83e09af9479babd9fce042856bd7006065515
hanan zatar <90716074+hananzatar2001@users.noreply.github.com>
2025-05-11 22:52:47 +0300

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -19,4 +19,8 @@ subprojects {
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
-id("com.google.gms.google-services") version "4.4.2" apply false
+plugins {
+    id("com.google.gms.google-services") version "4.4.2" apply false
+
+}
+
