f0d2888bb87cf3ae140bde1b4d322aaf92835af0
Danaaldajane <100998246+Danaaldajane@users.noreply.github.com>
2025-05-26 06:46:18 +0300

    isolation page ingredient screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,5 @@
 import 'package:cookmate/frontend/screens/ingredient_screen.dart';
 import 'package:flutter/material.dart';
-import 'frontend/widgets/bottom_navigation_bar.dart';
-import 'frontend/screens/daily_nutrition.dart';
-import 'frontend/screens/meal_planning.dart';
-import 'frontend/screens/calorie_tracking.dart';
-import 'frontend/screens/settings_screen.dart';
 import 'backend/services/database_service.dart';
 import 'firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -32,16 +27,7 @@ class MyApp extends StatelessWidget {
         visualDensity: VisualDensity.adaptivePlatformDensity,
         fontFamily: 'Poppins',
       ),
-      home: const BottomNavBar(),
-      routes: {
-        '/home': (context) => const DailyNutritionScreen(),
-        '/meal-planning': (context) => const MealPlanningScreen(),
-        '/calorie-tracking': (context) => const CalorieTrackingPage(),
-        '/recipe2': (context) => const RecipeDetailScreen(),
-        '/settings': (context) => const SettingsScreen(),
-        '/recipe': (context) => const RecipeDetailScreen(),
-      },
-      initialRoute: '/',
+      home: const RecipeDetailScreen(),
     );
   }
 }
