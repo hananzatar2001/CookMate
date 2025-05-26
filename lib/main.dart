c89e27fa0d1be002abf4775e9c42c489138819a5
Danaaldajane <100998246+Danaaldajane@users.noreply.github.com>
2025-05-26 07:03:06 +0300

    isolation page settings screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,9 @@
-import 'package:cookmate/frontend/screens/ingredient_screen.dart';
 import 'package:flutter/material.dart';
-import 'frontend/widgets/bottom_navigation_bar.dart';
-import 'frontend/screens/daily_nutrition.dart';
-import 'frontend/screens/meal_planning.dart';
-import 'frontend/screens/calorie_tracking.dart';
-import 'frontend/screens/settings_screen.dart';
 import 'backend/services/database_service.dart';
 import 'firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
+
+import 'frontend/screens/settings_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -32,16 +28,7 @@ class MyApp extends StatelessWidget {
         visualDensity: VisualDensity.adaptivePlatformDensity,
         fontFamily: 'Poppins',
       ),
-      home: const BottomNavBar(),
-      routes: {
-        '/home': (context) => const DailyNutritionScreen(),
-        '/meal-planning': (context) => const MealPlanningScreen(),
-        '/calorie-tracking': (context) => const CalorieTrackingPage(),
-        '/recipe2': (context) => const RecipeDetailScreen(),
-        '/settings': (context) => const SettingsScreen(),
-        '/recipe': (context) => const RecipeDetailScreen(),
-      },
-      initialRoute: '/',
+      home: const SettingsScreen(),
     );
   }
 }
