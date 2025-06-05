12513ab8309965162f164a3849cdf137bf3e231e
hanan zatar <90716074+hananzatar2001@users.noreply.github.com>
2025-06-05 14:01:18 +0300

    Update recipe_model.dart

--- lib/backend/models/recipe_model.dart
+++ lib/backend/models/recipe_model.dart
@@ -1,3 +1,4 @@
+/*
 class Recipe {
   final String userId;
   final String title;
@@ -46,4 +47,35 @@ class Recipe {
       fats: fats ?? this.fats,
     );
   }
+}*/
+
+
+class Recipe {
+  final String? recipeId;
+  final String userId;
+  final String title;
+  final List<String> steps;
+  final List<Map<String, dynamic>> Ingredients;
+  final String type;
+  final String? image_url;
+  final DateTime? date;
+  final num calories;
+  final num protein;
+  final num carbs;
+  final num fats;
+
+  Recipe({
+    this.recipeId,
+    required this.userId,
+    required this.title,
+    required this.steps,
+    required this.Ingredients,
+    required this.type,
+    this.image_url,
+    this.date,
+    required this.calories,
+    required this.protein,
+    required this.carbs,
+    required this.fats,
+  });
 }
